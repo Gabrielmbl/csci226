c54e0408853f7112a8a076c5ca422cd9bf1f499e
Gabrielmbl <gabrielmiranda7822@gmail.com>
2021-12-17 21:48:53 -0500

    Added login.php

---
+++ login.php
@@ -0,0 +1,10 @@
+<?php
+	//from: https://www.youtube.com/watch?v=YaZJZ01MKP4
+	//modified by Gabriel Lucena, 2021
+	echo '<h2>Type in Username and Password and press Return.</h2>';
+?>
+<form method="post">
+	<input type="text" name="login">
+	<input type="password" name="password">
+	<input type="submit" name="action" value="Return">
+</form>
